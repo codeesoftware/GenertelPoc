cc20079968e3a8e806a70607a9841f943855bdf8
Gábor Páll <codeesoftware@gmail.com>
2019-02-10 22:52:57 +0100

    unit

---
+++ GenertelPoc.Presentation/ClientApp/components/wizard/home/FirstPage.spec.js
@@ -0,0 +1,19 @@
+import { expect } from 'chai'
+import { mount } from '@vue/test-utils'
+import FirstPage from './FirstPage.vue'
+
+describe('FirstPage.vue', () => {
+    it('has a root element with class foo', () => {
+        // const wrapper = mount(FirstPage, {
+        //     propsData: {
+        //         pageId: 1
+        //     }
+        // })
+
+        // const textInput = wrapper.find('text-input')
+        // //textInput.('click')
+        // expect(wrapper.vm.pageId).toBe(1)
+        // expect(wrapper.is('.foo')).to.equal(true)
+        // expect(wrapper.vm.$data.msg).to.equal('Welcome to Your Vue.js App');
+    })
+})
--- GenertelPoc.Presentation/ClientApp/store/modules/offer.js
+++ GenertelPoc.Presentation/ClientApp/store/modules/offer.js
@@ -3,24 +3,16 @@ export default {
     state: {
         offerState: Object
     },
-    // getters: {
-    //     getPageByPageId: (state) => (pageId) => {
-    //         return state.offerState.pages.find(
-    //             page => page.pageId == 1
-    //         );
-    //     }
-    // },
+
     mutations: {
         setOfferState(state, offerState) {
-            console.table('setOfferState');
             state.offerState = offerState;
         },
         setPageState(state, { pageState, pageId }) {
-            console.table('setPageState');
+            debugger
             let page = state.offerState.pages.find(
                 page => page.pageId == pageId
             );
-            console.log(pageId);
             page = Object.assign(page, pageState);
         }
     },
