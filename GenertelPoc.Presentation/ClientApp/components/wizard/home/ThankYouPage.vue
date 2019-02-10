126a3b55cb970a91b44be524b3e9b72514f69c4c
Gábor Páll <codeesoftware@gmail.com>
2019-02-11 00:41:24 +0100

    missed

---
+++ GenertelPoc.Presentation/ClientApp/components/wizard/home/ThankYouPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="text-xs-center">
+      <h1 class="display-3">Adatok elküldve.</h1>
+      <p class="lead">
+        <strong>Köszönjök, hogy minket választott!!</strong>
+      </p>
+    </div>
+    <h4></h4>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "third-page"
+};
+</script>
+
+<style>
+</style>
