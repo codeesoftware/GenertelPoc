8deb70aaee4749bfba4e6f1a42f7c42b148bfd69
Gábor Páll <codeesoftware@gmail.com>
2019-02-14 23:22:53 +0100

    w

--- GenertelPoc.Presentation/ClientApp/components/wizard/home/SecondPage.vue
+++ GenertelPoc.Presentation/ClientApp/components/wizard/home/SecondPage.vue
@@ -1,20 +1,16 @@
 <template>
   <div>
-    {{errors}}
     <own-input
       v-model="pageViewModel.password.value"
       :property="pageViewModel.password"
       v-validate="'confirmed:confirmation'"
     ></own-input>
     <input v-model="password_confirm" type="password" ref="confirmation">
-    {{this.$refs}}
   </div>
 </template>
 
 <script>
 import Input from "../controls/Input.vue";
-
-// For example, you may want to add an error related to authentication:
 
 export default {
   name: "second-page",
