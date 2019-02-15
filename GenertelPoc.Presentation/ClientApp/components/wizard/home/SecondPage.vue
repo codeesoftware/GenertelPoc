17e5673d264f535e61de288db6505fa23a4daa61
Gábor Páll <codeesoftware@gmail.com>
2019-02-15 14:28:28 +0100

    slot

--- GenertelPoc.Presentation/ClientApp/components/wizard/home/SecondPage.vue
+++ GenertelPoc.Presentation/ClientApp/components/wizard/home/SecondPage.vue
@@ -1,50 +1,55 @@
 <template>
   <div class="form-group">
-    <label for="email">{{pageViewModel.email.label}}</label>
-    <input
-      type="text"
-      v-validate.continues.initial="pageViewModel.email.validationsString"
-      name="email"
-      class="form-control"
-      v-model.lazy.trim="pageViewModel.email.value"
-    >
+    <block :label="pageViewModel.email.label" name="email">
+      <input
+        slot="body"
+        type="text"
+        v-validate.continues.initial="pageViewModel.email.validationsString"
+        name="email"
+        class="form-control"
+        v-model.lazy.trim="pageViewModel.email.value"
+      >
+    </block>
     <error :errors="errors.collect('email')"></error>
 
-    <label for="password_confirmation">{{pageViewModel.password.label}}</label>
-    <input
-      type="password"
-      v-validate.continues.initial="pageViewModel.password.validationsString"
-      name="password_confirmation"
-      class="form-control"
-      ref="confirmation"
-    >
+    <block :label="pageViewModel.password.label" name="password_confirmation">
+      <input
+        slot="body"
+        type="password"
+        v-validate.continues.initial="pageViewModel.password.validationsString"
+        name="password_confirmation"
+        class="form-control"
+        ref="confirmation"
+      >
+    </block>
     <error :errors="errors.collect('password_confirmation')"></error>
 
-    <label for="password">{{pageViewModel.password.label}} ismét</label>
-    <input
-      type="password"
-      v-validate.continues.initial="pageViewModel.password.validationsString+'|VAL_300:confirmation'"
-      name="password"
-      class="form-control"
-      v-model.trim="pageViewModel.password.value"
-    >
+    <block :label="pageViewModel.password.label+' ismét'" name="password">
+      <input
+        slot="body"
+        type="password"
+        v-validate.continues.initial="pageViewModel.password.validationsString+'|VAL_300:confirmation'"
+        name="password"
+        class="form-control"
+        v-model.trim="pageViewModel.password.value"
+      >
+    </block>
     <error :errors="errors.collect('password')"></error>
-
   </div>
 </template>
 
 <script>
 import Error from "../controls/Error.vue";
+import Block from "../controls/Block.vue";
 
 export default {
   name: "second-page",
   components: {
-    Error
+    Error,
+    Block
   },
   data() {
-    return{
-
-    }
+    return {};
   },
   props: {
     pageViewModel: {
